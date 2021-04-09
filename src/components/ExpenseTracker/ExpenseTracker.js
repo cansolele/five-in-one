4a69cfc80afdd8037f857e1c39beeeffe6efe1d1
Victor Shamshin <androiddevelop@yandex.ru>
2021-04-09 20:04:44 +0300

    ET layout, responsive

--- src/components/ExpenseTracker/ExpenseTracker.js
+++ src/components/ExpenseTracker/ExpenseTracker.js
@@ -1,11 +1,32 @@
-import s from"./ExpenseTracker.module.css";
-const ExpenseTracker = () =>{
-    return(
-        <div>
-            <div className={s.ETFormContainer}>
-            <h1 className={s.appName}>Expense Tracker</h1>
-            </div>
-        </div>
-    )
-}
+import s from "./ExpenseTracker.module.css";
+import { IconContext } from "react-icons";
+import { ImPlus } from "react-icons/im";
+const ExpenseTracker = () => {
+  return (
+    <div>
+      <div className={s.ETFormContainer}>
+        <h1 className={s.appName}>Expense Tracker</h1>
+        <form className={s.etForm}>
+          <div className={s.inputContainer}>
+            <p>Name:</p>
+            <input type="text" className={s.input} />
+          </div>
+          <div className={s.inputContainer}>
+            <p>Price:</p>
+            <input type="text" className={s.input} />
+          </div>
+          <div className={s.inputContainer}>
+            <p>Date:</p>
+            <input type="date" className={s.input} />
+          </div>
+          <button type="submit" className={s.addItem}>
+            <IconContext.Provider value={{ className: s.addBtnIcon }}>
+              <ImPlus />
+            </IconContext.Provider>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default ExpenseTracker;
